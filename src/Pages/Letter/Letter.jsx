54de5a6f02b54fa73c434bd0ce6cb62055faea38
SUMIT <sumit.k@cdac.in>
2024-05-31 15:17:43 +0530

    feat: wbl letter

--- src/Pages/Letter/Letter.jsx
+++ src/Pages/Letter/Letter.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import "./Letter.css";
 
 function Letter() {
+  const name = "Sumit Kumar";
+  const certificateno = "CDACP/2024/ACTS/WBL/00254";
+  const currentDate = new Date();
+  const options = {
+    weekday: "short",
+    month: "short",
+    day: "2-digit",
+    year: "numeric",
+  };
+  const formattedDate = currentDate.toLocaleDateString("en-US", options);
+
   const handlePrint = () => {
     window.print();
   };
@@ -10,7 +21,7 @@ function Letter() {
       <div
         className="container"
         id="printContainer"
-        style={{ height: "100%", width: "100%"}}
+        style={{ height: "100%", width: "100%" }}
       >
         <div className="top" style={{ height: "200px", width: "100%" }}></div>
 
@@ -20,31 +31,35 @@ function Letter() {
             className="col-md-8"
             style={{ fontSize: "16px", textAlign: "justify" }}
           >
-            <p>CDACP/2024/ACTS/WBL/00254</p>
+            <p>{certificateno}</p>
             <div className="mt-5">
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Accusantium, magnam reiciendis illo impedit deleniti cumque
-                expedita praesentium dicta consectetur saepe voluptatem,
-                doloribus qui eos sapiente pariatur. Praesentium totam, ad
-                nesciunt rem, maxime sint reiciendis et aperiam aut omnis a,
-                vero atque cupiditate eos odio iure. Rem fuga reprehenderit
-                cupiditate voluptatum.
+                Lorem ipsum dolor sit, amet consectetur{" "}
+                <span className="fw-bold">{name}</span> elit. Accusantium,
+                magnam reiciendis illo impedit deleniti cumque expedita
+                praesentium dicta consectetur saepe voluptatem, doloribus qui
+                eos sapiente pariatur. Praesentium totam, ad nesciunt rem,
+                maxime sint reiciendis et aperiam aut omnis a, vero atque
+                cupiditate eos odio iure. Rem fuga reprehenderit cupiditate
+                voluptatum.
               </p>
             </div>
             <div className="mt-4">
               <p>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Accusantium, magnam reiciendis illo impedit deleniti cumque
-                expedita praesentium dicta consectetur saepe voluptatem,
-                doloribus qui eos sapiente pariatur. Praesentium totam, ad
-                nesciunt rem, maxime sint reiciendis et aperiam aut omnis a,
-                vero atque cupiditate eos odio iure. Rem fuga reprehenderit
-                cupiditate voluptatum.
+                Accusantium, magnam reiciendis illo{" "}
+                <span className="fw-bold">{name}</span> deleniti cumque expedita
+                praesentium dicta consectetur saepe voluptatem, doloribus qui
+                eos sapiente pariatur. Praesentium totam, ad nesciunt rem,
+                maxime sint reiciendis et aperiam aut omnis a, vero atque
+                cupiditate eos odio iure. Rem fuga reprehenderit cupiditate
+                voluptatum.
               </p>
             </div>
             <div className="mt-4">
-              <p>Date:</p>
+              <p>
+                Date: <span>{formattedDate}</span>
+              </p>
             </div>
             <div className="mt-4">
               <p>Signature:</p>
@@ -55,9 +70,8 @@ function Letter() {
         <div className="footer text-center" style={{ height: "100px" }}>
           <hr />
           <small className="">
-            CONTACT US lorem5
-            Innovation Park, Rajapul, Pashan, Patna - 411 008, Maharashtra
-            (India) Phone: +91-20-25503100 Fax: +91-20-25503131
+            CONTACT US lorem5 Innovation Park, Rajapul, Pashan, Patna - 411 008,
+            Patna (India) Phone: +91-20-******* Fax: +91-20-*******
           </small>
         </div>
       </div>
